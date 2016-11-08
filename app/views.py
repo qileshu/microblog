5bc6f520c0afb514e0ea4377213c41ea1f8d89c0
yanzhi <qileshu@163.com>
2016-11-08 16:41:07 +0800

    flask_demo_0.1

---
+++ app/views.py
@@ -0,0 +1,31 @@
+from app import app
+from flask import render_template, flash, redirect
+from .forms import LoginForm
+
+# index view function suppressed for brevity
+
+@app.route("/login", methods = ['GET', 'POST'])
+
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template("login.html", title = 'Sign In', form = form)
+
+
+@app.route("/index")
+def index():
+    #fake user
+    user = {'nickname': 'yanzhi'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html", user = user, posts = posts)

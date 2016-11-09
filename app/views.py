59b551ff1bd847b5a061d733cc6308a15838ff49
yanzhi <qileshu@163.com>
2016-11-09 19:40:11 +0800

    database finished

--- app/views.py
+++ app/views.py
@@ -5,13 +5,14 @@ from .forms import LoginForm
 # index view function suppressed for brevity
 
 @app.route("/login", methods = ['GET', 'POST'])
-
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for OpenID="' + form.openid.data + '", remember_me' + str(form.remember_me.data))
         return redirect('/index')
-    return render_template("login.html", title = 'Sign In', form = form)
+    return render_template("login.html", title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
 
 
 @app.route("/index")
--- config.py
+++ config.py
@@ -1,2 +1,16 @@
+#coding=utf-8
+import os
+# __file__:acquire the path of the module, maybe a relative path 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Flask-SQLAlchemy需要，是数据库文件的路径
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    { 'name': 'douban', 'url': 'https://accounts.douban.com/login'},
+    { 'name': 'xiami', 'url': 'https://login.xiami.com/member/login'}]
